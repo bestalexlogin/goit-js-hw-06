239ad207ac7760bcdc5c1b57f93aca3b92374af3
bestalexlogin <never.fade.ever@gmail.com>
2023-10-22 22:51:16 +0300

    homework 2

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const userData = {};
+
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  if (userData.email === '' || userData.password === '') {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    console.log(userData);
+    form.reset();
+  }
+});
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorButton = widget.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
